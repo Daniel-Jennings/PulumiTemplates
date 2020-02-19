0ec2ef8d22bbdec32702b8462a51fb4544079bdd
Daniel Jennings <daniel.jennings@trapezegroup.com>
2020-02-19 14:29:50 -0500

    Added minimum azure csharp template

---
+++ azure-cs-minimum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Pulumi;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+using PulumiFactory;
+class Program
+{
+    static Task<int> Main()
+    {
+        return Deployment.RunAsync(() => {
+
+            var config = new Pulumi.Config();
+            var companyCode = config.Require("company_code");
+            var location = config.Require("location");
+            var environment = config.Require("environment");
+            ResourceFactory factory = new ResourceFactory(companyCode, location, environment);
+
+            // Create an Azure Resource Group
+            var resourceGroup = factory.GetResourceGroup("00");
+
+            // Create an Azure Storage Account
+            var storageAccount = factory.GetStorageAccount("00", resourceGroup.Name);
+
+            // Export the connection string for the storage account
+            return new Dictionary<string, object?>
+            {
+                { "connectionString", storageAccount.PrimaryConnectionString },
+            };
+        });
+    }
+}
