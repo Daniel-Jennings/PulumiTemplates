52e88737a753796e5fb087099325eed38f5ef37d
Daniel Jennings <daniel.jennings@trapezegroup.com>
2020-03-17 20:59:14 -0400

    Removed Infra class, updated program to run deployment with additional outputs

--- sub-tcms-pul-infra/Program.cs
+++ sub-tcms-pul-infra/Program.cs
@@ -1,7 +1,35 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Pulumi;
+using PulumiFactory;
 
 class Program
 {
-    static Task<int> Main() => Deployment.RunAsync<Infra>();
+    static Task<int> Main()
+    {
+        return Deployment.RunAsync(async () => {
+            var config = new Pulumi.Config();
+            var location = config.Require("location");
+            var companyCode = config.Require("company_code");
+            var environment = config.Require("environment");
+            var scope = config.Require("default_scope");
+
+            ResourceFactory factory = new ResourceFactory(companyCode, location, environment, scope);
+
+            // Create the tags to be applied to these resources: scope
+            Dictionary<string, string> tags = new Dictionary<string, string>();
+            tags.Add("scope", scope);
+
+            // Create an Azure Resource Group and storage account
+            var resourceGroup = factory.GetResourceGroup(tags: tags);
+            var storageAccount = factory.GetStorageAccount(resourceGroup.Name, tags: tags);
+
+            return new Dictionary<string, object?>
+            {
+                { "connectionString", storageAccount.PrimaryConnectionString },
+                { "resourceGroupName", resourceGroup.Name },
+                { "resourceGroupId", resourceGroup.Id }
+            };
+        });
+    }
 }
